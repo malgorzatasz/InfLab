c4475abca7d4b035373e18bd47d45363fff09304
malgorzatasz <gosia.szalajda@gmail.com>
2019-11-06 13:25:40 +0100

    The Gilded Rose - refactoring update

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #refactored code from source "https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/master/python/gilded_rose.py"
-
+    
 def quality_positive_value(item):
     return item.quality > 0
 
@@ -27,7 +27,7 @@ def item_quality_decrease_twice(item):
 
 def item_quality_decrease(item):
     return item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-    
+        
 def item_backstage_passes(item):
     if item.name == "Backstage passes to a TAFKAL80ETC concert":
         if item.sell_in < 11:
@@ -38,6 +38,19 @@ def item_backstage_passes(item):
             item.quality = item.quality - item.quality
     return item.quality
 
+def decrease_quality(item):
+    if item_quality_decrease(item):
+        if can_be_bought(item):
+            decrease_quality_value(item)
+            item_quality_decrease_twice(item)
+    else: 
+        increase_quality_value(item)
+        item_backstage_passes(item)
+
+def update_sell_in(item):
+    if can_be_bought(item):
+        item.sell_in = item.sell_in - 1
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -45,25 +58,10 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item_quality_decrease(item):
-                if can_be_bought(item):
-                    decrease_quality_value(item)
-                    item_quality_decrease_twice(item)
-            else: 
-                increase_quality_value(item)
-                item_backstage_passes(item)
-
-            if can_be_bought(item):
-                item.sell_in = item.sell_in - 1 
+            decrease_quality(item)
+            update_sell_in(item)
             if overdue_item(item): 
-                if item_quality_decrease(item):
-                    if can_be_bought(item):
-                        decrease_quality_value(item)
-                        item_quality_decrease_twice(item)
-                else: 
-                    increase_quality_value(item)
-                    item_backstage_passes(item)
-
+                decrease_quality(item)
 
 class Item:
     def __init__(self, name, sell_in, quality):
