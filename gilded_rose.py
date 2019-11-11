29ff44aef3a6cccc695d270631ee0322c247035c
malgorzatasz <gosia.szalajda@gmail.com>
2019-11-11 18:30:10 +0100

    update - refactoring The Gilded Rose

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,25 +1,13 @@
 # -*- coding: utf-8 -*-
 #refactored code from source "https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/master/python/gilded_rose.py"
     
-def quality_positive_value(item):
-    return item.quality > 0
-
-def quality_max_value(item):
-    return item.quality < 50
-
-def overdue_item(item):
-    return item.sell_in < 0
-
 def decrease_quality_value(item):
-    if quality_positive_value(item):
+    if item.quality > 0:
         item.quality -= 1
 
 def increase_quality_value(item):
-    if quality_max_value(item):
+    if item.quality < 50:
         item.quality += 1
-
-def can_be_bought(item):
-    return item.name != "Sulfuras, Hand of Ragnaros"
 
 def item_quality_decrease_twice(item):
     if "Conjured " in item.name:
@@ -28,27 +16,27 @@ def item_quality_decrease_twice(item):
 def item_quality_decrease(item):
     return item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         
-def item_backstage_passes(item):
+def update_backstage_passes(item):
     if item.name == "Backstage passes to a TAFKAL80ETC concert":
         if item.sell_in < 11:
             increase_quality_value(item)
         if item.sell_in < 6:
             increase_quality_value(item)
-        if overdue_item(item):
+        if item.sell_in<0:
             item.quality = item.quality - item.quality
     return item.quality
 
 def decrease_quality(item):
     if item_quality_decrease(item):
-        if can_be_bought(item):
+        if item.name != "Sulfuras, Hand of Ragnaros":
             decrease_quality_value(item)
             item_quality_decrease_twice(item)
     else: 
         increase_quality_value(item)
-        item_backstage_passes(item)
+        update_backstage_passes(item)
 
 def update_sell_in(item):
-    if can_be_bought(item):
+    if item.name != "Sulfuras, Hand of Ragnaros":
         item.sell_in = item.sell_in - 1
 
 class GildedRose(object):
@@ -60,7 +48,7 @@ class GildedRose(object):
         for item in self.items:
             decrease_quality(item)
             update_sell_in(item)
-            if overdue_item(item): 
+            if item.sell_in < 0: 
                 decrease_quality(item)
 
 class Item:
